7af155d8588915f4811454e235bf4eecdf74f58c
Adam Mahood <mahood.adam@gmail.com>
2026-06-13 21:46:26 -0400

    feat(db): stub entities to allow app to build

--- app/src/main/java/io/github/asmahood/ledger/data/db/LedgerDatabase.kt
+++ app/src/main/java/io/github/asmahood/ledger/data/db/LedgerDatabase.kt
@@ -2,6 +2,13 @@ package io.github.asmahood.ledger.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import io.github.asmahood.ledger.data.db.entity.BudgetEntity
+import io.github.asmahood.ledger.data.db.entity.CategoryEntity
+import io.github.asmahood.ledger.data.db.entity.TransactionEntity
 
-@Database(entities = [], version = 1, exportSchema = true)
-abstract class LedgerDatabase : RoomDatabase() {}
+@Database(
+    entities = [TransactionEntity::class, CategoryEntity::class, BudgetEntity::class],
+    version = 1,
+    exportSchema = true
+)
+abstract class LedgerDatabase : RoomDatabase()
